f4a5361c7dcb5000b5430647e65432562ab6973c
FABIO ARIAS <fharias@gmail.com>
2022-06-21 20:59:28 -0500

    Implement config.Config Fixes #1

--- pkg/config/Config.go
+++ pkg/config/Config.go
@@ -1,6 +1,11 @@
 package config
 
-import "github.com/edgaralexanderfr/cyber-kill-server/pkg/types"
+import (
+	"fmt"
+
+	"github.com/edgaralexanderfr/cyber-kill-server/pkg/types"
+	"github.com/pmylund/go-cache"
+)
 
 type Config struct {
 	Port types.Port `json:"port"`
@@ -8,4 +13,21 @@ type Config struct {
 	Server struct {
 		TickRate byte `json:"tickRate"`
 	} `json:"server"`
+}
+
+func (config Config) GetConfig(cacheable bool) *Config {
+	fmt.Println(cacheable)
+	if cacheable {
+		c := cache.New(cache.NoExpiration, cache.NoExpiration)
+		config, found := c.Get("config")
+		if found {
+			fmt.Println("Found in cache")
+			fmt.Println(config)
+			return config.(*Config)
+		} else {
+			c.Set("config", &config, cache.NoExpiration)
+		}
+	}
+	fmt.Println("Not found in cache")
+	return &config
 }
--- pkg/config/ConfigFactory.go
+++ pkg/config/ConfigFactory.go
@@ -1,7 +1,19 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type ConfigFactory struct{}
 
 func (configFactory *ConfigFactory) NewConfig() ConfigInterface {
-	return nil
+	var c Config
+	configFile, err := os.Open("config.json")
+	if err != nil {
+		return c
+	}
+	defer configFile.Close()
+	json.NewDecoder(configFile).Decode(&c)
+	return c
 }

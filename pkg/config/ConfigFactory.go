2e7f61cb2a842b9e3554cf5ed0b1004e198e436a
FABIO ARIAS <fharias@gmail.com>
2022-06-22 17:57:31 -0500

    Nueva Implementaciond e Config

--- pkg/config/ConfigFactory.go
+++ pkg/config/ConfigFactory.go
@@ -1,19 +1,7 @@
 package config
 
-import (
-	"encoding/json"
-	"os"
-)
-
 type ConfigFactory struct{}
 
 func (configFactory *ConfigFactory) NewConfig() ConfigInterface {
-	var c Config
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		return c
-	}
-	defer configFile.Close()
-	json.NewDecoder(configFile).Decode(&c)
-	return c
+	return &Config{}
 }

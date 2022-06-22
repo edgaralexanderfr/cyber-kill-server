c127238f425d3f315da830ceeb12068222d2e12d
FABIO ARIAS <fharias@gmail.com>
2022-06-22 17:57:07 -0500

    Nueva implementacion de Config

--- pkg/config/Config.go
+++ pkg/config/Config.go
@@ -1,9 +1,14 @@
 package config
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/edgaralexanderfr/cyber-kill-server/pkg/types"
 	"github.com/pmylund/go-cache"
 )
+
+var c = cache.New(cache.NoExpiration, cache.NoExpiration)
 
 type Config struct {
 	Port types.Port `json:"port"`
@@ -14,8 +19,15 @@ type Config struct {
 }
 
 func (config Config) GetConfig(cacheable bool) *Config {
+	if &config == nil {
+		configFile, err := os.Open("config.json")
+		if err != nil {
+			return nil
+		}
+		defer configFile.Close()
+		json.NewDecoder(configFile).Decode(&config)
+	}
 	if cacheable {
-		c := cache.New(cache.NoExpiration, cache.NoExpiration)
 		config, found := c.Get("config")
 		if found {
 			return config.(*Config)

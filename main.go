a607494731191645c32b9c1922f6ab8295a71914
FABIO ARIAS <fharias@gmail.com>
2022-06-23 12:19:26 -0500

    Implement config.Config Fixes #1

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"github.com/edgaralexanderfr/cyber-kill-server/pkg/config"
-)
+import "github.com/edgaralexanderfr/cyber-kill-server/pkg/game"
 
 func main() {
-	//gameFactory := game.GameFactory{}
-	//gameFactory.NewGame().Run()
-	configFactory := config.ConfigFactory{}
-	config := configFactory.NewConfig().GetConfig(true)
-	println(config.Port, config.Server.TickRate)
+	gameFactory := game.GameFactory{}
+	gameFactory.NewGame().Run()
 }

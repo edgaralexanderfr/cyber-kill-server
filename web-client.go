8c8385a132e301d66bc44263cbfde3aa1f0e6ad0
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2022-07-13 23:40:32 -0500

    Integrate Three.js into Web Client with example.

--- web-client.go
+++ web-client.go
@@ -1,11 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"syscall/js"
 )
 
+var (
+	global                js.Value
+	requestAnimationFrame js.Value
+	scene                 js.Value
+	camera                js.Value
+	renderer              js.Value
+	cube                  js.Value
+	animateFunc           js.Func
+)
+
 func main() {
-	fmt.Println(`Running`, js.Global().Get("document").Get("title"), `...`)
-	fmt.Println(`Executed at:`, js.Global().Get("Date").New().Call("toDateString"))
+	global = js.Global()
+	requestAnimationFrame = global.Get("requestAnimationFrame")
+	window := global.Get("window")
+	document := global.Get("document")
+	body := document.Get("body")
+	THREE := global.Get("THREE")
+
+	innerWidth := window.Get("innerWidth").Int()
+	innerHeight := window.Get("innerHeight").Int()
+
+	scene = THREE.Get("Scene").New()
+	camera = THREE.Get("PerspectiveCamera").New(75, innerWidth/innerHeight, 0.1, 1000)
+
+	renderer = THREE.Get("WebGLRenderer").New()
+	renderer.Call("setSize", innerWidth, innerHeight)
+	body.Call("appendChild", renderer.Get("domElement"))
+
+	geometry := THREE.Get("BoxGeometry").New(1, 1, 1)
+	material := THREE.Get("MeshBasicMaterial").New(map[string]interface{}{
+		"color": 0x00ff00,
+	})
+
+	cube = THREE.Get("Mesh").New(geometry, material)
+	scene.Call("add", cube)
+
+	camera.Get("position").Set("z", 5)
+
+	animateFunc = js.FuncOf(animate)
+
+	animate(js.Value{}, nil)
+	select {}
+}
+
+func animate(this js.Value, args []js.Value) interface{} {
+	requestAnimationFrame.Invoke(animateFunc)
+	renderer.Call("render", scene, camera)
+
+	cube.Get("rotation").Set("x", cube.Get("rotation").Get("x").Float()+0.01)
+	cube.Get("rotation").Set("y", cube.Get("rotation").Get("y").Float()+0.01)
+
+	return nil
 }
